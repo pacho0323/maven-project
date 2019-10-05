3d2e0ddaee4340b8087003a1388c4ac69e8c38eb
Juan Francisco <pacho0323@hotmail.com>
2019-10-05 17:44:48 -0500

    corregir errores

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,21 +1,10 @@
 package com.example;
 
-/**
- * This is a class.
- */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
   public Greeter() {
 
   }
-  /**
-   * @param el nombre de una persona
-   * @return devuelve un string
-   */
-  
   public final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
